259053ad1e00dfdcc1ad55360fe75ba0155d51be
Rochele Edenís <rochele.edenis@gmail.com>
2024-02-11 16:51:35 -0300

    refactor: use camel case in SoilChemicalAnalysis attributes

--- app/Domain/Entity/SoilChemicalAnalysis.php
+++ app/Domain/Entity/SoilChemicalAnalysis.php
@@ -9,20 +9,20 @@ class SoilChemicalAnalysis
     use MagicMethodsTrait;
 
     public function __construct(
-        protected float $soil_type,
-        protected float $hydrogen_potential,
+        protected float $soilType,
+        protected float $hydrogenPotential,
         protected float $phosphorus,
         protected float $potassium,
         protected float $calcium,
         protected float $magnesium,
         protected float $aluminum,
-        protected float $base_sum,
-        protected float $effective_ctc,
-        protected float $potential_acidity,
-        protected float $cation_exchange_capacity_at_ph_7,
-        protected float $base_saturation,
-        protected float $aluminum_saturation,
-        protected float $organic_matter,
+        protected float $baseSum,
+        protected float $effectiveCtc,
+        protected float $potentialAcidity,
+        protected float $cationExchangeCapacityAtPh7,
+        protected float $baseSaturation,
+        protected float $aluminumSaturation,
+        protected float $organicMatter,
     ) {
 
     }
