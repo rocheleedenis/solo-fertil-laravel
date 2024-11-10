3aa4acf66d71fd729a892e4372fe356e37edd36d
Rochele Edenís <rochele.edenis@gmail.com>
2024-11-09 22:32:33 -0300

    fix: remove hardcoded values from AnaliseSeeder

--- database/seeders/AnaliseSeeder.php
+++ database/seeders/AnaliseSeeder.php
@@ -12,42 +12,6 @@ class AnaliseSeeder extends Seeder
      */
     public function run(): void
     {
-        $keys = [
-            'id',
-            'data',
-            'local',
-            'profundidade',
-            'pH',
-            'fosforo',
-            'potassio',
-            'calcio',
-            'magnesio',
-            'aluminio',
-            'somaBases',
-            'acidezPotencial',
-            'matOrganica',
-            'prem',
-            'teorArgila',
-            'idProdutor',
-            'idUsuario') VALUES
-[
-    33,
-'2017-03-27',
-'Talhão01',
-30,
-'5.32',
-'12.80',
-'200.00',
-'2.99',
-'0.81',
-'0.00',
-'4.31',
-'3.74',
-'2.77',
-'22.90',
-'28.98',
-21,
-50];
-
+        //
     }
 }
